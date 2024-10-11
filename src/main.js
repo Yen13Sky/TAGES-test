07e633c49d89bcf9cc16809fcae29fc43274a1a5
Yen13Sky <Yen13Sky@gmail.com>
2024-10-11 08:04:57 +0300

    test-finished-build

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-// import store from './store'
 import '../../../../../src/styles/style.scss'
 const pinia = createPinia()
 createApp(App).use(pinia).use(router).mount('#app')
